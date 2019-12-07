f65d76fab53c88f1daf8596d447e7500a0b7da35
David Wu <55110205+wuwei-hx@users.noreply.github.com>
2019-12-07 16:01:52 +0800

    add onResponse param

--- src/createFetchSaga.ts
+++ src/createFetchSaga.ts
@@ -2,13 +2,36 @@ import { put, call, apply, select } from "redux-saga/effects";
 
 const createFetchSaga = ({
   onFetch,
+  onResponse,
   onSuccess = response => response,
   onFailure = error => error,
   asyncActions,
   getIsFetching,
-  getToken
-}) =>
-  function* fetchSaga(action) {
+  getToken,
+  verbose = false
+}) => {
+  function* handleResponse(response: Response) {
+    if (!response.ok) {
+      // statuscode = 400代表用户名、密码错误
+      // todo: 确认一下符合http status code含义
+      let error;
+      if (response.status === 401) error = { error: "操作未授权" };
+      else if (response.status === 500)
+        error = { error: "无法连接服务器，请稍后重试", meta: "notOK" };
+      else error = { error: `${response.status}: ${response.statusText}` };
+
+      yield put(asyncActions.failure(onFailure(error)));
+      return;
+    }
+
+    console.log("calling apply...");
+    const respJson = yield apply(response, response.json, []);
+    console.log("respJson", respJson);
+    console.log("end calling apply...");
+    yield put(asyncActions.success(onSuccess(respJson)));
+  }
+
+  return function* fetchSaga(action) {
     if (getIsFetching && (yield select(getIsFetching))) return;
 
     yield put(asyncActions.request(action.payload));
@@ -17,34 +40,25 @@ const createFetchSaga = ({
     // 如果捕捉，那么网络及其它错误会捕捉到，由本级处理
     // 不捕捉，只处理response内的错误信息
     try {
-      // const resp = yield* fetchCall(action);
       const token = getToken ? yield select(getToken) : undefined;
       const resp = yield call(onFetch, action, token);
 
-      /// test code
-      console.log("resp", resp);
-      /// end of test
+      if (verbose) console.log("resp", resp);
 
-      if (!resp.ok) {
-        // statuscode = 400代表用户名、密码错误
-        // todo: 确认一下符合http status code含义
-        let error;
-        if (resp.status === 401) error = { error: "操作未授权" };
-        else if (resp.status === 500)
-          error = { error: "无法连接服务器，请稍后重试", meta: "notOK" };
-        else error = { error: `${resp.status}: ${resp.statusText}` };
+      if (onResponse && typeof onResponse === "function") {
+        const result = yield* onResponse(resp);
 
-        // yield call(onFailure, error);
-        yield put(asyncActions.failure(onFailure(error)));
-        return;
+        if (result && result.error) {
+          yield put(asyncActions.failure(onFailure(result)));
+          return;
+        }
+
+        if (result) {
+          yield put(asyncActions.success(onSuccess(result)));
+          return;
+        }
       }
-
-      console.log("calling apply...");
-      const respJson = yield apply(resp, resp.json, []);
-      console.log("respJson", respJson);
-      console.log("end calling apply...");
-      yield put(asyncActions.success(onSuccess(respJson)));
-      // yield call(onSuccess, respJson);
+      yield* handleResponse(resp);
     } catch (err) {
       yield put(
         asyncActions.failure(
@@ -61,5 +75,5 @@ const createFetchSaga = ({
       console.log("err", err);
     }
   };
-
+};
 export default createFetchSaga;
